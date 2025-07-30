fa24595c30488446b385e2b45e3f765e1c300d70
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-30 02:32:52 +0200

    adding migration jobs to the config

---
+++ k8s/run_migrations_city.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for CityStore DB..."
+until mysqladmin ping -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --silent; do
+  sleep 1
+done
+
+echo "Creating citystore_db if it doesn't exist..."
+mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
+
+echo "Running CityStore migrations..."
+mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" citystore_db < city_store/data/migrations/1_init.up.sql
+
+echo "✅ All migrations complete."
--- k8s/up.sh
+++ k8s/up.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+set -e
+
+source .env
+
+kubectl create secret generic citystore-db-creds \
+  --from-literal=CITYSTORE_DB_USER="${CITYSTORE_DB_USER}" \
+  --from-literal=CITYSTORE_DB_PASSWORD="${CITYSTORE_DB_PASSWORD}"
+
+kubectl create secret generic centralstore-db-creds \
+  --from-literal=CENTRALSTORE_DB_USER="${CENTRALSTORE_DB_USER}" \
+  --from-literal=CENTRALSTORE_DB_PASSWORD="${CENTRALSTORE_DB_PASSWORD}"
+
 kubectl create secret generic citystore-db-password \
   --from-file=MYSQL_ROOT_PASSWORD_FILE=../passwords/citystore_db_password.txt
 
@@ -9,11 +22,19 @@ kubectl apply -f configs/central-config.yaml
 kubectl apply -f configs/city-db-config.yaml
 kubectl apply -f configs/central-db-config.yaml
 kubectl apply -f volumes.yaml
+
 kubectl apply -f deployments.yaml
+
 kubectl apply -f services.yaml
-sleep 1
+
+kubectl apply -f citystore-db-migrations-job.yaml
+kubectl apply -f centralstore-db-migrations-job.yaml
+
+
+kubectl wait --for=condition=complete job/citystore-db-migrator-job --timeout=300s
+kubectl wait --for=condition=complete job/centralstore-db-migrator-job --timeout=300s
+
 kubectl apply -f ingress.yaml
 
-# clear
-echo "Starting"
+echo "Starting dashboard..."
 minikube dashboard
