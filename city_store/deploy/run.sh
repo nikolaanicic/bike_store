0e91b7278d1ddc3403864ba7b8a9b7824ec3d64a
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-30 04:32:12 +0200

    invalid ssl command

--- city_store/deploy/run.sh
+++ city_store/deploy/run.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
 set -e
 
 sleep 20
 echo "Creating citystore_db if it doesn't exist..."
-mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl-mode=DISABLED -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
+mariadb -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl=0 -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
 
 echo "Running CityStore migrations..."
-mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl-mode=DISABLED citystore_db < 1_init.up.sql
+mariadb -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl=0 citystore_db < 1_init.up.sql
 
 echo "✅ All migrations complete. exiting..."
 
