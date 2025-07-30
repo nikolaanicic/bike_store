9da27cb499a7015b5192c5084cb808cf5dffde2a
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-30 03:54:37 +0200

    trying without ping

--- k8s/run_migrations_city.sh
+++ k8s/run_migrations_city.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-echo "Waiting for CityStore DB..."
-until mysqladmin ping -h localhost -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --silent; do
-  sleep 1
-done
-
+sleep 20
 echo "Creating citystore_db if it doesn't exist..."
 mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
 
 echo "Running CityStore migrations..."
 mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" citystore_db < city_store/data/migrations/1_init.up.sql
 
-echo "✅ All migrations complete."
+echo "✅ All migrations complete. exiting..."
+exit
