da1a9c690de200ad1f095f204d77bfcae8c57686
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-30 04:16:35 +0200

    changing the docker image

--- city_store/deploy/run.sh
+++ city_store/deploy/run.sh
@@ -7,7 +7,9 @@ echo "Creating citystore_db if it doesn't exist..."
 mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
 
 echo "Running CityStore migrations..."
-mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" citystore_db < city_store/data/migrations/1_init.up.sql
+mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" citystore_db < 1_init.up.sql
 
 echo "✅ All migrations complete. exiting..."
-exit
+
+echo "Starting the backend server"
+./citystore_backend
