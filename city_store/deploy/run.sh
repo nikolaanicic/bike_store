310b58b74bc6185d8f01ef7df6ba73f1c6318905
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-30 04:27:15 +0200

    disabling ssl on migrations

--- city_store/deploy/run.sh
+++ city_store/deploy/run.sh
@@ -4,10 +4,10 @@ set -e
 
 sleep 20
 echo "Creating citystore_db if it doesn't exist..."
-mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
+mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl-mode=DISABLED -e "CREATE DATABASE IF NOT EXISTS citystore_db;"
 
 echo "Running CityStore migrations..."
-mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" citystore_db < 1_init.up.sql
+mysql -h citystore-db -u"$CITYSTORE_DB_USER" -p"$CITYSTORE_DB_PASSWORD" --ssl-mode=DISABLED citystore_db < 1_init.up.sql
 
 echo "✅ All migrations complete. exiting..."
 
